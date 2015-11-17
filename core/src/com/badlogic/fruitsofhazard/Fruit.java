e7e08c1d557119acbfa63fc905ce44f143ab6c1d
stephcornick <stephc88733@aol.com>
2015-11-17 16:06:24 -0500

    made Fruit extend Sprite, still need to adjust fruit subclasses accordingly

--- core/src/com/badlogic/fruitsofhazard/Fruit.java
+++ core/src/com/badlogic/fruitsofhazard/Fruit.java
@@ -1,8 +1,55 @@
 package com.badlogic.fruitsofhazard;
 
-public class Fruit
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import static com.badlogic.gdx.graphics.g2d.SpriteBatch.*;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.NumberUtils;
+
+
+public class Fruit extends Sprite
 {
-    private String direction;
+
+    public Fruit()
+    {
+        super();
+    }
+
+    public Fruit(Texture texture)
+    {
+        super(texture);
+    }
+
+    public Fruit(Texture texture, int srcWidth, int srcHeight)
+    {
+        super(texture, srcWidth, srcHeight);
+    }
+
+    public Fruit(Texture texture, int srcX, int srcY, int srcWidth, int srcHeight)
+    {
+        super(texture, srcX, srcY, srcWidth, srcHeight);
+    }
+
+    public Fruit(TextureRegion region)
+    {
+        super(region);
+    }
+
+    public Fruit(TextureRegion region, int srcX, int srcY, int srcWidth, int srcHeight)
+    {
+        super(region, srcX, srcY, srcWidth, srcHeight);
+    }
+
+    public Fruit(Sprite sprite)
+    {
+        super(sprite);
+    }
+
+   /* private String direction;
     private float xChord;
     private float yChord;
     //some kind of image variable should go here
@@ -62,7 +109,7 @@ public class Fruit
     public void setNext(Fruit f)
     {
         next = f;
-    }
+    }*/
 
 }
 
--- core/src/com/badlogic/fruitsofhazard/PlayerOrange.java
+++ core/src/com/badlogic/fruitsofhazard/PlayerOrange.java
@@ -1,5 +1,14 @@
 package com.badlogic.fruitsofhazard;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import static com.badlogic.gdx.graphics.g2d.SpriteBatch.*;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.NumberUtils;
 /**
  * Created by s0928018 on 11/6/15.
  */
@@ -7,13 +16,13 @@ public class PlayerOrange extends Fruit
 {
     private int score;
     private int health;
+    private Texture orangeImage = new Texture(Gdx.files.internal("PlayerOrange.png"));
 
     PlayerOrange()
     {
-        super();
+        super(orangeImage);
         score = 0;
         health = 3;
-        //image = orange image
     }
     PlayerOrange(float x, float y, int s, int h)
     {
