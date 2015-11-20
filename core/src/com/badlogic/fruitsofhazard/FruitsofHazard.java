e2774ee50e90eced49afcd45c45b7ed7f0b5863f
stephcornick <stephc88733@aol.com>
2015-11-20 15:53:14 -0500

    started screening

--- core/src/com/badlogic/fruitsofhazard/FruitsofHazard.java
+++ core/src/com/badlogic/fruitsofhazard/FruitsofHazard.java
@@ -10,27 +10,62 @@ package com.badlogic.fruitsofhazard;
 //Working Directory...[Location on harddrive]/desktop/FruitsOfHazard/android/assets
 //Use classpath of module: desktop
 
-		import java.util.Iterator;
+		//import java.util.Iterator;
 
-		import com.badlogic.gdx.ApplicationListener;
-		import com.badlogic.gdx.Gdx;
-        import com.badlogic.gdx.Input;
-        import com.badlogic.gdx.Input.Keys;
-		import com.badlogic.gdx.audio.Music;
-		import com.badlogic.gdx.audio.Sound;
-		import com.badlogic.gdx.graphics.GL20;
-		import com.badlogic.gdx.graphics.OrthographicCamera;
-		import com.badlogic.gdx.graphics.Texture;
+		//import com.badlogic.gdx.ApplicationListener;
+		//import com.badlogic.gdx.Gdx;
+       // import com.badlogic.gdx.Input;
+       // import com.badlogic.gdx.Input.Keys;
+		//import com.badlogic.gdx.Screen;
+		//import com.badlogic.gdx.audio.Music;
+	//	import com.badlogic.gdx.audio.Sound;
+	//	import com.badlogic.gdx.graphics.GL20;
+	//	import com.badlogic.gdx.graphics.OrthographicCamera;
+	//	import com.badlogic.gdx.graphics.Texture;
+	//	import com.badlogic.gdx.graphics.g2d.BitmapFont;
+	//	import com.badlogic.gdx.graphics.g2d.Sprite;
+	//	import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+	//	import com.badlogic.gdx.math.MathUtils;
+	//	import com.badlogic.gdx.math.Rectangle;
+		//import com.badlogic.gdx.utils.Array;
+		//import com.badlogic.gdx.utils.TimeUtils;
+//
+
+
+		import com.badlogic.gdx.Game;
+
+//package com.badlogic.fruitsofHazard;
 		import com.badlogic.gdx.graphics.g2d.BitmapFont;
-		import com.badlogic.gdx.graphics.g2d.Sprite;
 		import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-		import com.badlogic.gdx.math.MathUtils;
-		import com.badlogic.gdx.math.Rectangle;
-		import com.badlogic.gdx.utils.Array;
-		import com.badlogic.gdx.utils.TimeUtils;
+
+public class FruitsofHazard extends Game {
+
+	SpriteBatch batch;
+	BitmapFont font;
+
+	public void create() {
+		batch = new SpriteBatch();
+		// Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+
+}
 
 
-public class FruitsofHazard implements ApplicationListener {
+//-------------
+
+public class FruitsofHazard implements Screen {
+	/*implements ApplicationListener*/
 
 	//Implement pause/resume
 	State state = State.RUN;
